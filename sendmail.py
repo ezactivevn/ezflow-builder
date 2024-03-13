8aec3c9430d2d667b28d2cc2edd775bd1d4a89d7
Phu Hoang <mchoang98@gmail.com>
2024-03-13 16:15:43 +0700

    update status if fail

--- sendmail.py
+++ sendmail.py
@@ -1,33 +1,25 @@
 import requests
-import mysql.connector
-from mysql.connector import Error
+import pusher
 import sys
 
 
 app_name = sys.argv[1]
 status = sys.argv[2]
 
-def update_db(app_name):
-    """Update database"""
-    connection = mysql.connector.connect(
-        host='35.241.69.119',
-        user='ezactive.phu',
-        password='wuA7Ms^F%1at',
-        auth_plugin='mysql_native_password',
-        database = 'ezactive_ezflow'
-    )
+response = requests.post("https://www.ezactive.com/ezflow/server/admin/customer/updateCustomerByAppID", {'app_id': app_name, 'status': status})
 
-    cursor = connection.cursor()
 
-    # update table customer field status
-    cursor.execute(f"UPDATE customer SET status = {status} WHERE name = '{app_name}'")
-    connection.commit()
-    cursor.close()
-    connection.close()
+pusher_client = pusher.Pusher(
+        app_id='1764419',
+        key='b82e6e4504e08d436c41',
+        secret='d923f53e1b98e0e15948',
+        cluster='ap1',
+        ssl=True
+        )
 
-    print("Database updated")
-
-update_db(app_name)
+pusher_client.trigger('manage-customer', 'warning-update', {
+    'message': f"Project {app_name} cannot be build. Failed to deploy. Please try again.",
+    })
 
     
 
