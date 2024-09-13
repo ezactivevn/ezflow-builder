732663993c1f2f98db067b6259cd65427021b542
Phu Hoang <mchoang98@gmail.com>
2024-09-13 22:13:26 +0700

    update build message

--- slack.py
+++ slack.py
@@ -2,6 +2,7 @@ import requests
 import json 
 import sys
 import datetime
+import pusher
 
 app_id = sys.argv[1]
 status = sys.argv[2] # 1 or 0
@@ -37,3 +38,19 @@ sad_text = f":sob::sob::sob: \n Project *{app_id}* has been build *{success}* on
 message_text = fun_text if status == "1" else sad_text
 # Call the function to send the message
 send_slack_message(slack_token, slack_channel, message_text)
+
+
+pusher_client = pusher.Pusher(
+        app_id='1764419',
+        key='b82e6e4504e08d436c41',
+        secret='d923f53e1b98e0e15948',
+        cluster='ap1',
+        ssl=True
+        )
+
+pusher_client.trigger('manage-customer', 'finish-deploy', {
+    'message': message_text,
+    'title': message_text,
+    'id': app_id,
+    })
+
