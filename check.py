6b8ceb9c916d0a283efe75295e0a1894d6e2810f
Phu Hoang <mchoang98@gmail.com>
2024-09-13 10:45:53 +0700

    update check limitation ezbuilder

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ total, used, free = shutil.disk_usage("/")
 percent_used = (used / total) * 100
 
 # Check if the available space is less than or equal to 10%
-if percent_used >= 70:
+if percent_used >= 90:
     print("full")
     sys.exit(1)  # Exit with a status code of 1 to indicate failure
 else:
