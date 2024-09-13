c6cbfb4b6c8fc4f4394df70acd85f30094f06131
Phu Hoang <mchoang98@gmail.com>
2024-09-13 16:20:27 +0700

    update pusher timer

---
+++ pusher_timer.py
@@ -0,0 +1,26 @@
+import pusher
+import time
+import os
+
+# Initialize Pusher client
+pusher_client = pusher.Pusher(
+    app_id='1764419',
+    key='b82e6e4504e08d436c41',
+    secret='d923f53e1b98e0e15948',
+    cluster='ap1',
+    ssl=True
+)
+
+# Get duration from environment variable, default to 300 seconds (5 minutes)
+duration = int(os.getenv('TIMER_DURATION', 300))
+
+start_time = time.time()
+
+while (time.time() - start_time) < duration:
+    pusher_client.trigger('manage-customer', 'deploy', 
+        {
+        'message': 'Finish loading',
+        'title': "Build App...",
+        'id': "helloworld",
+     })
+    time.sleep(3)
